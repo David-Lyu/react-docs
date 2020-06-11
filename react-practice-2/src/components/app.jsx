cbdb485cab49ad4700780f0b53ee888e100556ba
David Lyu <lyu.david@yahoo.com>
2020-06-11 16:28:39 -0700

    created a basic layout

--- react-practice-2/src/components/app.jsx
+++ react-practice-2/src/components/app.jsx
@@ -5,6 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
+        <h1>Tic Tac Toe</h1>
         <Board></Board>
       </div>
     )
--- react-practice-2/src/components/board.jsx
+++ react-practice-2/src/components/board.jsx
@@ -2,20 +2,22 @@ import React from "react"
 import Square from "./square"
 
 export default class Board extends React.Component {
-
-  getSquares() {
-    this.squares = [];
-  }
-
-  renderSquare(i) {
-    return <Square value={i}/>
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares = 9
+    }
   }
 
   render() {
+    const boardArea = []
+    for(i = 0; i < this.state.squares; i++) {
+      boardArea.push(<Square id={i} value={i}/>)
+    }
 
     return (
       <div className="board">
-        {this.renderSquare(9)}
+        {boardArea}
       </div>
     )
   }

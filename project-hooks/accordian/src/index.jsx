6d5c9f1899122a40271b87f2175a749917e79653
David <lyu.david@yahoo.com>
2020-07-15 11:49:54 -0700

    finsihed accordian app with no styling

--- project-hooks/accordian/src/index.jsx
+++ project-hooks/accordian/src/index.jsx
@@ -2,7 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
+const languages = [
+    {
+        language: "Hypertext Markup Language",
+        detail: "Lorem",
+        id: 0
+    },
+    {
+        language: "Cascading Style Sheets",
+        detail: "Lorem",
+        id: 1
+    },
+    {
+        language: "JavaScript",
+        detail: "Lorem",
+        id: 2
+    },
+]
+
 ReactDOM.render(
-    <App/>,
+    <App languages={languages}/>,
     document.getElementById('root')
 )
